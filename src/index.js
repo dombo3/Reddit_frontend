d5195f19cd6763d09ad569bd8f05c49a5d9453dd
Peter Dombovari <dombo.peter@gmail.com>
2020-12-15 19:23:22 +0100

    list the posts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
 import API_URL from './config';
-import './style.css';
 
 const main = document.querySelector('main');
 
+getPosts();
 
-console.log(API_URL);
+function elapsedDaysFrom(timestamp) {
+  const fromDate = new Date(timestamp);
+  const currentDate = new Date();
+  const difference = currentDate - fromDate;
+  return Math.floor(difference / 1000 / 60 / 60 / 24);
+}
+
+function getPosts() {
+  fetch(API_URL + "/posts")
+    .then(response => response.json())
+    .then(posts => {
+      posts
+        .map(post => createPost(post.score, post.title, elapsedDaysFrom(post.timestamp), post.author || "anonymus"))
+        .forEach(post => main.appendChild(post));
+    });
+}
+
+function createPost(vote, title, days, author) {
+  const article = document.createElement('article');
+  article.setAttribute('class', 'post');
+  
+  const div1 = document.createElement('div');
+  div1.setAttribute('class', 'vote-box');
+  
+  const img1 = document.createElement('img');
+  img1.src = 'img/upvote.png';
+  img1.classList.add('upvote');
+  
+  const p1 = document.createElement('p');
+  p1.setAttribute('class', 'vote');
+  p1.textContent = vote;
+  
+  const img2 = document.createElement('img');
+  img2.setAttribute('src', 'img/downvote.png');
+  img2.setAttribute('class', 'downvote');
+  
+  const div2 = document.createElement('div');
+  div2.setAttribute('class', 'content-box');
+
+  const h3 = document.createElement('h3');
+  h3.setAttribute('class', 'post-title');
+  h3.textContent = title;
+
+  const p2 = document.createElement('p');
+  p2.setAttribute('class', 'info');
+  p2.textContent = `submitted ${days} days ago by ${author}`;
+
+  const a1 = document.createElement('a');
+  a1.setAttribute('class', 'link');
+  a1.setAttribute('href', '#');
+  a1.textContent = 'modify';
+
+  const a2 = document.createElement('a');
+  a2.setAttribute('class', 'link');
+  a2.setAttribute('href', '#');
+  a2.textContent = 'remove';
+
+  div1.appendChild(img1);
+  div1.appendChild(p1);
+  div1.appendChild(img2);
+  
+  div2.appendChild(h3);
+  div2.appendChild(p2);
+  div2.appendChild(a1);
+  div2.appendChild(a2);
+
+  article.appendChild(div1);
+  article.appendChild(div2);
+
+  return article;
+};
